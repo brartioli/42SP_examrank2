8c041241ff538f1916bb78fb0c218df1193b62ef
Bruna Fernandez Artioli <bfernan2@student.42sp.org.br>
2025-09-02 14:17:54 -0300

    feat: add fizzbuzz ok

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -6,7 +6,35 @@
 /*   By: bfernan2 <bfernan2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 13:21:55 by bfernan2          #+#    #+#             */
-/*   Updated: 2025/09/02 13:21:56 by bfernan2         ###   ########.fr       */
+/*   Updated: 2025/09/02 14:15:11 by bfernan2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void ft_putnbr(int nb)
+{
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+	}
+	write (1, &"0123456789"[nb % 10], 1);
+}
+int main ()
+{
+	int i = 0;
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			write (1, "fizzbuzz", 8);
+		else if (i % 3 == 0)
+			write (1, "fizz", 4);
+		else if (i % 5 == 0)
+			write (1, "buzz", 4);
+		else
+			ft_putnbr(i);
+		i++;
+		write (1, "\n", 2);
+	}
+	return 0;
+}
